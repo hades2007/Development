3f90e6f06c9e2b1b594e409c7667a24c6b7c6eeb
hades2007 <87477676+hades2007@users.noreply.github.com>
2021-07-17 00:54:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ const dateFormat = require('dateformat');
 
 
 
-const prefix = "" //prefix e bot dabni 
-const developers = "" // id  acwnte xot dabni
+const prefix = "p!" //prefix e bot dabni 
+const developers = "847884920147804250" // id  acwnte xot dabni
 
 
 
 
 
-client.login("");//tokne bot dabne
+client.login("ODY1Mjc4MzAxODI3NzYwMTMw.YPBrNw.driC2b0tbSmerQz35cFhfN1f7Dk");//tokne bot dabne
